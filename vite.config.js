618b94649b391e17b65ed35dccaa535abfc58e91
chieh <secret12501250@gmail.com>
2024-08-13 11:21:16 +0800

    chore: 更新 base 路徑為 '/2024-hexschool-web-layout-training-week4-vite/'

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/2024-hexschool-web-layout-training-vite-week4/",
+  base: "/2024-hexschool-web-layout-training-week4-vite/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
